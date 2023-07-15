e1e6c88e0e6113b4001caa3ce7a93578a65bbcb3
hagersameh111 <hager_sharaf236@yahoo.com>
2023-07-15 12:48:19 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * first prog
+ * main f
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
