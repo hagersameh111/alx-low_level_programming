433eceb4dc91661b075c727ee61001f3a3ab53ca
hagersameh111 <hager_sharaf236@yahoo.com>
2023-07-19 09:51:19 -0700

    1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- nums2
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
