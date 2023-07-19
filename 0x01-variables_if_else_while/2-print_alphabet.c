8ada94a65b8f44ee5ad68051313cc3402e1d3690
hagersameh111 <hager_sharaf236@yahoo.com>
2023-07-19 08:19:37 -0700

    1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- alpha
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
