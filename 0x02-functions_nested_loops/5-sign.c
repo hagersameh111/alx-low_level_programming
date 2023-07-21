3664ad09c441a978d338f44f0a5a901b8b074e1d
hagersameh111 <hager_sharaf236@yahoo.com>
2023-07-21 11:15:25 -0700

    .02

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,25 +2,23 @@
 /**
  * print_sign- priont sign
   * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
  *  @n: the int to check
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 	return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-	return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 	return (-1);
+	}
+	else
+	{
+		_putchar(48);
+	return (0);
 	}
 }
