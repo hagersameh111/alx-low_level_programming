96f5f9647bab98c4f8329966e93f335d49102576
hagersameh111 <hager_sharaf236@yahoo.com>
2023-07-21 17:54:32 -0700

    p

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,12 @@
  */
 void print_most_numbers(void)
 {
-	int n;
+	char c;
 
-	for (n >= 0; n <= 9; n++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (n != 2 || n != 4)
-			_putchar(n + '0');
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
